dd7e1d8e4c397c094319040173d3f04a32a3edf0
olehmartynenko <martynenkooleg77@gmail.com>
2023-05-29 20:41:08 +0300

    Add balancer and health check

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"hash/fnv"
 	"io"
 	"log"
 	"net/http"
@@ -14,20 +15,21 @@ import (
 )
 
 var (
-	port = flag.Int("port", 8090, "load balancer port")
+	port       = flag.Int("port", 8090, "load balancer port")
 	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	https      = flag.Bool("https", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
 var (
-	timeout = time.Duration(*timeoutSec) * time.Second
+	timeout     = time.Duration(*timeoutSec) * time.Second
 	serversPool = []string{
 		"server1:8080",
 		"server2:8080",
 		"server3:8080",
 	}
+	healthyPool = make([]string, len(serversPool))
 )
 
 func scheme() string {
@@ -88,22 +90,37 @@ func main() {
 	flag.Parse()
 
 	// TODO: Використовуйте дані про стан сервреа, щоб підтримувати список тих серверів, яким можна відправляти ззапит.
-	for _, server := range serversPool {
-		server := server
-		go func() {
-			for range time.Tick(10 * time.Second) {
-				log.Println(server, health(server))
-			}
-		}()
-	}
+	healthCheck(serversPool, healthyPool)
 
 	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// TODO: Рееалізуйте свій алгоритм балансувальника.
-		forward(serversPool[0], rw, r)
+		forward(healthyPool[getIndex(r.RemoteAddr)], rw, r)
 	}))
 
 	log.Println("Starting load balancer...")
 	log.Printf("Tracing support enabled: %t", *traceEnabled)
 	frontend.Start()
 	signal.WaitForTerminationSignal()
+}
+
+func getIndex(address string) int {
+	hash := fnv.New32()
+	hash.Write([]byte(address))
+	hashed := int(hash.Sum32())
+	serverIndex := hashed % len(healthyPool)
+	return serverIndex
+}
+
+func healthCheck(servers []string, result []string) {
+	for i, server := range servers {
+		server := server
+		i := i
+		go func() {
+			for range time.Tick(10 * time.Second) {
+				if health(server) {
+					result[i] = server
+				}
+				log.Println(server, health(server))
+			}
+		}()
+	}
 }
